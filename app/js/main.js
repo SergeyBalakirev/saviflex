a516690183bacf8bfb37de72eb585da4f17fd8b3
SergeyBalakirev <balakirev.sergey87@gmail.com>
2020-10-04 22:03:54 +0300

    added tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function () {
 
-
+  $(".wrapper .tab").click(function () {
+    $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+    $(".tab_item").hide().eq($(this).index()).fadeIn();
+  }).eq(0).addClass("active");
 
 });
