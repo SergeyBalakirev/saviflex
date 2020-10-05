e56a30bfb8ec43050367313e26d72a6b23b85cdd
SergeyBalakirev <balakirev.sergey87@gmail.com>
2020-10-05 22:22:45 +0300

    doing adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function () {
 
+
+  function f1() {
+    let out1 = document.querySelector('.header-top__products');
+
+    out1.classList.toggle('height');
+  }
+
+  document.querySelector('.header-top__link--down').onclick = f1;
+
+  $('.header-banner__slider-inner').slick({
+    arrows: false,
+  });
+
+
+
   $(".wrapper .tab").click(function () {
     $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
     $(".tab_item").hide().eq($(this).index()).fadeIn();
